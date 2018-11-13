176577e82acb48fb11c38c33df77bef0608af596
Arun Sood <arsood@gmail.com>
2018-11-13 12:03:46 -0800

    Sidebar with places

--- src/ProwlFinder.js
+++ src/ProwlFinder.js
@@ -6,7 +6,18 @@ import locations from "./locations.json";
 import cat from "./assets/img/cat-icon.png";
 
 class ProwlFinder extends Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showSidebar: true,
+      locations: locations
+    };
+
+    this.toggleSidebar = this.toggleSidebar.bind(this);
+  }
+
+  renderMap(mapLocations) {
     const map = new window.google.maps.Map(
       this.refs.googleMap,
       {
@@ -36,14 +47,42 @@ class ProwlFinder extends Component {
     map.fitBounds(bounds);
   }
 
+  componentDidMount() {
+    this.renderMap(this.state.locations);
+  }
+
+  toggleSidebar() {
+    this.setState({
+      showSidebar: this.state.showSidebar ? false : true
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div className="sidebar">
-          Hi
-        </div>
+        { this.state.showSidebar ?
+          <div className="sidebar">
+            <i onClick={this.toggleSidebar} className="fa fa-remove close-sidebar"></i>
+
+            <ul className="sidebar-list">
+              { this.state.locations.map((location) => {
+                return (
+                  <li>
+                    <div className="text-bold">
+                      {location.name}
+                    </div>
+                    <div>
+                      {location.features}
+                    </div>
+                  </li>
+                );
+              }) }
+            </ul>
+          </div>
+        : null }
+
         <div className="header">
-          <img src={cat} className="brand-logo" /> <span>ProwlFinder</span>
+          <img onClick={this.toggleSidebar} src={cat} className="brand-logo" /> <span>ProwlFinder</span>
         </div>
         <div ref="googleMap" className="google-map"></div>
       </React.Fragment>
